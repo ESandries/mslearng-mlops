a87e983afdbb13a79511829db1fccd67e8827ec5
ESadries emanuel@sandries.com <emanuel@sandries.com>
2023-08-13 02:46:38 -0600

    I completed the "Challenge 0: Convert a notebook to production code"

--- src/model/train.py
+++ src/model/train.py
@@ -7,12 +7,16 @@ import os
 import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+import mlflow
+
 
 
 # define functions
 def main(args):
-    # TO DO: enable autologging
-
+    # TODO: enable autologging
+    mlflow.autolog()
 
     # read data
     df = get_csvs_df(args.training_data)
@@ -33,7 +37,13 @@ def get_csvs_df(path):
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
 
-# TO DO: add function to split data
+# TODO: add function to split data
+
+def split_data(df):
+    # split data
+    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+
 
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
